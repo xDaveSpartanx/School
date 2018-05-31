d5b2be8a5a41d356e5749726d27d0609869ade2c
davidrios <davidrios@172.20.1.231>
2018-05-31 16:47:24 -0400

    Added Constructors and Member Methods per UML.

--- cop2800/src/week03/Hangman.java
+++ cop2800/src/week03/Hangman.java
@@ -9,26 +9,71 @@ public class Hangman {
 	
 	
 	
+	public String Hangman() {
+		
+	}
+	
+	public boolean isCorrectLetter(char) {
+		
+	}
+	
+	public String isCorrectWord(String) {
+		
+	}
+	
+	public boolean isWinner() {
+		
+	}
+	
+	
+	
+	
 	// Public Methods
 	
 	// Protected Methods
 	
 	// Private Methods
+	
+private void initialize() {
 		
+	}
+	
+	private void trace(String) {
+		
+	}
+	
 	// Public Methods
 	
+	public char getM_incorrectLetters() {
+		return m_incorrectLettersGuessed;
+	}
+	public StringBuilder getM_mysteryWordWithDashes() {
+		return m_mysteryWordWithDashes;
+	}
+	public String get_MysteryWord() {
+		return m_currentMysteryWord;
+	}
+	public int getM_NextMysteryWord() {
+		return m_currentMysteryWordIndex++;
+	}
+	
+	
+	
+		
 	// Public Constants
 		public static final int MAX_GUESSES = 7;
 		
+		
+	}
 		// Private data
-		private String m_currentMysteryWord;
+		private String m_currentMysteryWord = "";
 		private int m_currentMysteryWordIndex = 0;
-		private String m_dicrtionary;
+		private String m_dictionary;
 		private char m_incorrectLettersGuessed;
 		private int m_incorrectLettersIndex;
 		private boolean m_isWinner;
 		private int maxMysteryWords = 0;
 		private StringBuilder m_mysteryWordWithDashes;
-		private int m_remainingGuesses = MAX_GUESSES;
+		private int m_remainingGuesses = MAX_GUESSES ;
 
 }

7e40adab3e0b4b129fb0cdac30bd58fe72404be6
David Rios <david.rios@pcnutz.com>
2018-06-10 23:18:57 -0400

    Updated Hangman with Professors code. Next Task Look for places to insert my own code.

--- cop2800/src/week03/Hangman.java
+++ cop2800/src/week03/Hangman.java
@@ -15,23 +15,26 @@ public class Hangman {
 
 		
 		// Public Methods
-	public boolean isCorrectLetter(char cc){
+	public boolean isCorrectLetter(char letter){
 		
 		boolean result = false;
-		int index =
-		int index = m_currentMysteryWord.indexOf(m_currentMysteryWordIndex);
-		if(index != m_mysteryWordWithDashes.indexOf(m_currentMysteryWord))
+		// m_currentMysteryWord letter index
+		int index = m_currentMysteryWord.indexOf(letter);
+		// If index does not equal m_mysteryWordWithDashes index letter
+		// 
+		if(index != -1)
 			{
-			m_mysteryWordWithDashes.trimToSize();
+			
+			m_mysteryWordWithDashes.replace(index, index+1, new String(new char[] {letter}));
 			result = true;
 			}
 		else
 		{
-			m_incorrectLettersGuessed[m_incorrectLettersIndex++]  ;
+			m_incorrectLettersGuessed[m_incorrectLettersIndex++] = letter;
 			m_remainingGuesses--;
 			result = false;
 		}
-			
+			return result;
 	}
 	
 	public boolean isCorrectWord(String word) {
@@ -49,32 +52,34 @@ public class Hangman {
 	// Private Methods
 	
 	private void initialize() {
-		
+		// sets 
+		m_maxMysteryWords = m_dictionary.length;
+		m_isWinner = false;
 	}
 	
-	private void trace() {
-		
+	private void trace(String msg) {
+		System.out.println(msg);
 	}
 
 	
 	// Public Methods
 	
-	public char[] getIncorrectLetters() {
-		return m_incorrectLettersGuessed;
+	public String getIncorrectLetters() {
+		return new String(m_incorrectLettersGuessed).trim();
 	}
 	public String getMysteryWord() {
 		return m_currentMysteryWord;
 	}
-	public StringBuilder getMysteryWordWithDashes() {
-		return m_mysteryWordWithDashes;
+	public String getMysteryWordWithDashes() {
+		return m_mysteryWordWithDashes.toString();
 	}
 	public boolean getNextMysteryWord() {
 			
 		boolean result = false;
 		
-		if(m_currentMysteryWordIndex < maxMysteryWords)
+		if(m_currentMysteryWordIndex < m_maxMysteryWords)
 		{
-			//get the next word ion the list, increment the index
+			//get the next word on the list, increment the index
 			m_currentMysteryWord = m_dictionary[m_currentMysteryWordIndex++];
 			
 			//size the array that holds incorrect guesses; can't be bigger than MAX_ TRIES
@@ -82,14 +87,19 @@ public class Hangman {
 			m_incorrectLettersIndex = 0;
 			m_isWinner = false;
 			
+			m_remainingGuesses = MAX_GUESSES;
+			
 			//build the dashed version of the mystery word
 			//this will get updated when a character is selected
-			m_mysteryWordWithDashes = new StringBulder();
+			m_mysteryWordWithDashes = new StringBuilder();
 			for(int i = 0; i < m_currentMysteryWord.length(); i++)
 			{
 				m_mysteryWordWithDashes.append("-");
 			}
+			
+			result = true;
 		}
+		return result;
 	}
  	public int getNumberOfGuessesLeft() {
 		return m_remainingGuesses;
@@ -108,7 +118,7 @@ public class Hangman {
 	private char[] m_incorrectLettersGuessed;
 	private int m_incorrectLettersIndex;
 	private boolean m_isWinner;
-	private int maxMysteryWords = 0;
+	private int m_maxMysteryWords = 0;
 	private StringBuilder m_mysteryWordWithDashes;
 	private int m_remainingGuesses = MAX_GUESSES ;
 
